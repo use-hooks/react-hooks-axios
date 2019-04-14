1782ac9e0e77c9b347170062c37d8078712cebdd
Cody Chan <int64ago@gmail.com>
2019-04-14 20:30:19 +0800

    test: add test

--- __tests__/index.js
+++ __tests__/index.js
@@ -1,7 +1,29 @@
-const hooks = require('../src/index').default;
+/* eslint-disable react/jsx-filename-extension */
+import '@babel/polyfill';
+import React from 'react';
+import MockAdapter from 'axios-mock-adapter';
+import { render, waitForElement } from 'react-testing-library';
+import useAxios, { axios } from '../src/index';
 
-describe('hooks test', () => {
-  it('should be a function', () => {
-    expect(hooks).toEqual(expect.any(Function));
+it('should be a function', () => {
+  expect(useAxios).toEqual(expect.any(Function));
+});
+
+// Known issue: https://github.com/kentcdodds/react-testing-library/issues/281
+it('use custom instance', async () => {
+  const mock = new MockAdapter(axios);
+  mock.onGet('/test').reply(200, {
+    foo: 'bar',
   });
+
+  function App() {
+    const { response } = useAxios({
+      axios: axios.create({}),
+      url: '/test',
+      trigger: '', // Auto trigger once
+    });
+    return <div>{((response || {}).data || {}).foo}</div>;
+  }
+  const { getByText } = render(<App />);
+  await waitForElement(() => getByText(/bar/));
 });
